971fffbec8ab863854e3f06f16a5f9f770975e02
GIIU <59857022+Cwwwwwx@users.noreply.github.com>
2023-06-07 18:29:25 +0800

    commit test (#1)
    巴拉巴拉

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /*
  * @Author: Chen
  * @Date: 2023-06-07 10:52:06
- * @LastEditTime: 2023-06-07 12:02:49
+ * @LastEditTime: 2023-06-07 12:21:43
  * @Description:
  */
 #include <iostream>
@@ -9,5 +9,7 @@
 int main()
 {
     std::cout << "Hello World";
+    std::cout << "git test!";
     std::cout << "main test";
 }
+
