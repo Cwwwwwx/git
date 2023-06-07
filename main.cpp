f8523746bd70ba146611a0a50f4d5bffa4127553
Chen <cwx2662@gmail.com>
2023-06-07 12:02:49 +0800

    aaa

---
+++ main.cpp
@@ -0,0 +1,12 @@
+/*
+ * @Author: Chen
+ * @Date: 2023-06-07 10:52:06
+ * @LastEditTime: 2023-06-07 12:02:49
+ * @Description:
+ */
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World";
+}
